9142945a0c3a2b9d5900e81d62562e7f66cee128
akash gupta <53353355+akash-guptaa@users.noreply.github.com>
2024-05-13 21:50:24 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,10 +1,10 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to = $_POST["recipient"];
-    $subject = $_POST["subject"];
-    $message = $_POST["message"];
-    $headers = "From: your_email@example.com\r\n";
-    $headers .= "Reply-To: your_email@example.com\r\n";
+    $to = "akashgupta2134@gmail.com";
+    $subject = "test mail";
+    $message = "test mail";
+    $headers = "From: akashgupta2134@gmail.com\r\n";
+    $headers .= "Reply-To: akashgupta2134@gmail.com\r\n";
     $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
     if(mail($to, $subject, $message, $headers)) {
